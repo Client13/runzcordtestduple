70a6e8018d6f3cd1ff7765bd88521cfd4f9edf6a
Client13 <76941677+Client13@users.noreply.github.com>
2022-05-22 10:15:26 +0530

    Add files via upload

---
+++ runzcord/types/__init__.py
@@ -0,0 +1,10 @@
+"""
+runzcord.types
+~~~~~~~~~~~~~~
+
+Typings for the Discord API
+
+:copyright: (c) 2015-2021 Rapptz, 2021-present runzcord Development
+:license: MIT, see LICENSE for more details.
+
+"""
